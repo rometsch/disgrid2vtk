90dc217155a80f515e0a6f797dbf9fa89d27f254
Thomas Rometsch <thomas.rometsch@gmail.com>
2022-05-17 17:09:54 +0200

    support specifying local directory

--- src/simdata2vtk/_command_line_.py
+++ src/simdata2vtk/_command_line_.py
@@ -1,6 +1,5 @@
 from argparse import ArgumentParser
-
-from simdata import NData
+from os.path import exists
 
 from simdata2vtk.convert import simdata2vtk
 
@@ -8,7 +7,8 @@ from simdata2vtk.convert import simdata2vtk
 def main():
 
     parser = ArgumentParser()
-    parser.add_argument("simid", type=str, help="Id of the simulation.")
+    parser.add_argument("resource", type=str,
+                        help="Path or id of the simulation.")
     parser.add_argument("Noutput", type=int, help="Output number to process.")
     parser.add_argument("-o", "--output", type=str,
                         help="Output file, can include '{}' to use as a template.")
@@ -16,11 +16,18 @@ def main():
                         help="Set flag to force update the data from the server.")
     opts = parser.parse_args()
 
+    if exists(opts.resource):
+        from simdata import Data
+        d = Data(opts.resource)
+        name = "data"
+    else:
+        from simdata import NData
+        d = NData(opts.resource)
+        name = opts.resource
+
     if opts.output is None:
-        filename = f"{opts.simid}_{opts.Noutput}"
+        filename = f"{name}_{opts.Noutput}"
     else:
         filename = opts.output.format(opts.Noutput)
-
-    d = NData(opts.simid)
 
     simdata2vtk(d, opts.Noutput, filename)
